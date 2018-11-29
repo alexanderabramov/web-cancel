196ca4d9701221ed0cfd3a19830fbf2044e28811
Alexander Abramov <alexander.abramov.pub@gmail.com>
2018-11-29 22:53:27 +0100

    ADO wait

---
+++ DotNetCancel/DotNetCancel.Web/WaitController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DotNetCancel.Web.Models;
+
+namespace DotNetCancel.Web
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaitController : ControllerBase
+    {
+        private readonly DotNetCancelWebContext _context;
+
+        public WaitController(DotNetCancelWebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Waits
+        [HttpGet()]
+        public async Task<IActionResult> Wait()
+        {
+			using (var connection = _context.Database.GetDbConnection())
+			{
+				connection.Open();
+				using (var command = connection.CreateCommand())
+				{
+					command.CommandText = "waitfor delay '00:00:30'";
+					await command.ExecuteNonQueryAsync();
+				}
+			}
+			return Ok();
+        }
+    }
+}
