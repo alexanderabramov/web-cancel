b6b63fd31141c1826b6d2cba442b0a3cda290265
Alexander Abramov <alexander.abramov.pub@gmail.com>
2018-11-30 00:05:16 +0100

    cancellationToken gets triggered under Kestrel, but not under IIS express
    Know bug that will probably never be fixed:
    https://github.com/aspnet/AspNetCoreModule/issues/38

--- DotNetCancel/DotNetCancel.Web/WaitController.cs
+++ DotNetCancel/DotNetCancel.Web/WaitController.cs
@@ -1,29 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DotNetCancel.Web.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using DotNetCancel.Web.Models;
+using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DotNetCancel.Web
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class WaitController : ControllerBase
-    {
-        private readonly DotNetCancelWebContext _context;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class WaitController : ControllerBase
+	{
+		private readonly DotNetCancelWebContext _context;
 
-        public WaitController(DotNetCancelWebContext context)
-        {
-            _context = context;
-        }
+		public WaitController(DotNetCancelWebContext context)
+		{
+			_context = context;
+		}
 
-        [HttpGet("andcancel")]
-        public async Task<IActionResult> WaitAndCancel()
-        {
+		[HttpGet("andcancel")]
+		public async Task<IActionResult> WaitAndCancel()
+		{
 			string message = "";
 			using (var connection = _context.Database.GetDbConnection())
 			{
@@ -44,7 +45,7 @@ namespace DotNetCancel.Web
 					catch (SqlException ex)
 					{
 						var sqlErrors = ex.Errors;
-						if (!(sqlErrors.Count==2 && sqlErrors[1].Message=="Operation cancelled by user."))
+						if (!(sqlErrors.Count == 2 && sqlErrors[1].Message == "Operation cancelled by user."))
 						{
 							throw;
 						}
@@ -55,5 +56,31 @@ namespace DotNetCancel.Web
 			return base.Ok(message);
 		}
 
+		[HttpGet("wait")]
+		public async Task<IActionResult> Wait(CancellationToken cancellationToken)
+		{
+			try
+			{
+				string message = "";
+				using (var connection = _context.Database.GetDbConnection())
+				{
+					connection.Open();
+					using (var command = connection.CreateCommand())
+					{
+						// synthetic long-running query
+						command.CommandText = "waitfor delay '00:00:30'";
+						Task<int> task = command.ExecuteNonQueryAsync(cancellationToken);
+
+						await task;
+						message = "Completed " + cancellationToken.IsCancellationRequested;
+					}
+				}
+				return base.Ok(message);
+			}
+			catch (Exception ex)
+			{
+				throw;
+			}
+		}
 	}
 }
